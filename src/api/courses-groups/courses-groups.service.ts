6305ac7253f161843074a6e6d759c7e29940e952
Leonid <l.gofshteyn@gmail.com>
2025-03-08 19:53:10 +0200

    Исправлены ошибки

--- src/api/courses-groups/courses-groups.service.ts
+++ src/api/courses-groups/courses-groups.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCoursesGroupDto } from './dto/create-courses-group.dto';
-import { UpdateCoursesGroupDto } from './dto/update-courses-group.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CoursesGroup } from './entities/courses-group.entity';
 import { plainToInstance } from 'class-transformer';
-import { Course } from '../courses/entities/course.entity';
 
 @Injectable()
 export class CoursesGroupsService {
 
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(createCoursesGroupDto: CreateCoursesGroupDto) {
-    return 'This action adds a new coursesGroup';
-  }
+  // create(createCoursesGroupDto: CreateCoursesGroupDto) {
+  //   return 'This action adds a new coursesGroup';
+  // }
 
   public async findAll(expand?: string[]) {
 
@@ -30,15 +27,15 @@ export class CoursesGroupsService {
     return plainToInstance(CoursesGroup, result);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} coursesGroup`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} coursesGroup`;
+  // }
 
-  update(id: number, updateCoursesGroupDto: UpdateCoursesGroupDto) {
-    return `This action updates a #${id} coursesGroup`;
-  }
+  // update(id: number, updateCoursesGroupDto: UpdateCoursesGroupDto) {
+  //   return `This action updates a #${id} coursesGroup`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} coursesGroup`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} coursesGroup`;
+  // }
 }
