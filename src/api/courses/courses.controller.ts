9a7a2be3d567316ea49c05de4f7a28ab331d985b
Leonid <l.gofshteyn@gmail.com>
2025-04-02 09:28:21 +0300

    Мелкие исправления

--- src/api/courses/courses.controller.ts
+++ src/api/courses/courses.controller.ts
@@ -6,28 +6,8 @@ import { Course } from './entities/course.entity';
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
-  // @Post()
-  // create(@Body() createCourseDto: CreateCourseDto) {
-  //   return this.coursesService.create(createCourseDto);
-  // }
-
   @Get()
   public async findAll(): Promise<Course[]> {
     return await this.coursesService.findAll();
   }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.coursesService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
-  //   return this.coursesService.update(+id, updateCourseDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.coursesService.remove(+id);
-  // }
 }
