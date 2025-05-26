3a10371a7f1ff9952d67b5ac553a01ac9dfa56d7
Leonid <l.gofshteyn@gmail.com>
2025-05-26 11:41:04 +0300

    Доработано api products-groups

--- src/api/products-groups/products-groups.controller.ts
+++ src/api/products-groups/products-groups.controller.ts
@@ -1,34 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { ProductsGroupsService } from './products-groups.service';
-import { CreateProductsGroupDto } from './dto/create-products-group.dto';
-import { UpdateProductsGroupDto } from './dto/update-products-group.dto';
+import { Request } from 'express';
 
 @Controller('api/products-groups')
 export class ProductsGroupsController {
   constructor(private readonly productsGroupsService: ProductsGroupsService) {}
 
-  @Post()
-  create(@Body() createProductsGroupDto: CreateProductsGroupDto) {
-    return this.productsGroupsService.create(createProductsGroupDto);
-  }
-
   @Get()
-  findAll() {
+  findAll(@Req() req: Request) {
+    if (req.query?.lang && typeof req.query.lang === 'string')
+      return this.productsGroupsService.findAllLocalized(req.query.lang);
     return this.productsGroupsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsGroupsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductsGroupDto: UpdateProductsGroupDto) {
-    return this.productsGroupsService.update(+id, updateProductsGroupDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsGroupsService.remove(+id);
-  }
 }
--- src/api/products-groups/products-groups.service.ts
+++ src/api/products-groups/products-groups.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductsGroupDto } from './dto/create-products-group.dto';
 import { UpdateProductsGroupDto } from './dto/update-products-group.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsGroupsService {
-  create(createProductsGroupDto: CreateProductsGroupDto) {
-    return 'This action adds a new productsGroup';
+
+  public async findAllLocalized(lang: string) {
+    const result = await this.findAll();
+    return result;
   }
 
   findAll() {
     return `This action returns all productsGroups`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} productsGroup`;
-  }
-
-  update(id: number, updateProductsGroupDto: UpdateProductsGroupDto) {
-    return `This action updates a #${id} productsGroup`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} productsGroup`;
-  }
 }
